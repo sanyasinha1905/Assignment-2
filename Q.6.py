5cb0f421046edfef256a316aa90b4e01198a228e
sanyasinha1905 <120155221+sanyasinha1905@users.noreply.github.com>
2022-12-22 20:15:42 +0530

    Delete Q.6.py

--- Q.6.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter the lenght of the side: "))
-b=int(input("Enter the lenght of the side: "))
-c=int(input("Enter the lenght of the side: "))
-if (a+b)>c and (a+c)>b and(b+c)>a:
-    print("Yes")
-else:
-    print("No")
